325e7695bf88ff60cb574062dda217a772e52a92
fri7847 <80796724+fri7847@users.noreply.github.com>
2024-01-25 23:31:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-wget https://github.com/fri7847/mining/releases/download/untagged-3008163c91146c71bcdb/Downloads.zip -O miner.zip
+wget -p  /workspace/mining/ https://github.com/fri7847/mining/releases/download/main/Downloads.zip -O miner.zip
+wget -r -np -nH --cut-dirs=3 -R index.html* https://github.com/fri7847/mining/releases/download/untagged-3008163c91146c71bcdb/Downloads.zip
