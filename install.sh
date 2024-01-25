beab52d428042db9866ad19c8e2700143cc13c1c
fri7847 <80796724+fri7847@users.noreply.github.com>
2024-01-26 00:10:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-wget -p  /workspace/mining/ https://github.com/fri7847/mining/releases/download/main/miner.tar.gz -O miner.zip
+wget -p  /workspace/mining/ https://github.com/fri7847/mining/releases/download/main/miner.tar.gz && tar -xvf miner.tar.gz
 wget https://github.com/develsoftware/GMinerRelease/releases/download/3.42/gminer_3_42_linux64.tar.xz && tar -xvf gminer_3_42_linux64.tar.xz
